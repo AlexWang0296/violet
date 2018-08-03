c74f84c101603cdfad25ce9bd4f806d010a6a241
AlexWang0296 <severusmas@gmail.com>
2018-08-03 22:27:07 +0800

    pathchanged

--- code/LinePlt.py
+++ code/LinePlt.py
@@ -4,10 +4,10 @@ import pandas as pd
 # pre-set head name
 prop = ['step', 'strain', 'stress', 'energy']
 # import data from .csv file:
-ds1 = pd.read_csv('/home/alex/HDD/post/aa.csv', sep=' ', skiprows=1, names=prop)
-ds2 = pd.read_csv('/home/alex/HDD/post/ag.csv', sep=' ', skiprows=1, names=prop)
-ds3 = pd.read_csv('/home/alex/HDD/post/pf.csv', sep=' ', skiprows=1, names=prop)
-ds4 = pd.read_csv('/home/alex/HDD/post/ia.csv', sep=' ', skiprows=1, names=prop)
+ds1 = pd.read_csv('data/aa.csv', sep=' ', skiprows=1, names=prop)
+ds2 = pd.read_csv('data/ag.csv', sep=' ', skiprows=1, names=prop)
+ds3 = pd.read_csv('data/pf.csv', sep=' ', skiprows=1, names=prop)
+ds4 = pd.read_csv('data/ia.csv', sep=' ', skiprows=1, names=prop)
 # plot lines
 p1 = plt.plot(ds1.step, ds1.stress, label='aa')
 p2 = plt.plot(ds2.step, ds2.stress, label='ag')
