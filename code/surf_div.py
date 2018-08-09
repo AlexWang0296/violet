61dab0e46b9de73124ca3eb0ac048083540af572
AlexWang0296 <severusmas@gmail.com>
2018-08-09 22:11:34 +0800

    add fig:1st div

---
+++ code/surf_div.py
@@ -0,0 +1,35 @@
+
+import os
+import os.path
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+WDIR = os.getcwd()
+loadpath = os.path.join(WDIR,'code','data','surf')
+writepath = os.path.join(WDIR,'draft','img')
+
+#plt.savefig(os.path.join(WDIR,'draft','img','surf.pdf'))
+#loadpath = os.path.join(WDIR,'code','data','surf')
+ag =np.loadtxt(os.path.join(loadpath,'surf_ag.txt'))
+ia =np.loadtxt(os.path.join(loadpath,'surf_ia.txt'))
+ig =np.loadtxt(os.path.join(loadpath,'surf_ig.txt'))
+pf =np.loadtxt(os.path.join(loadpath,'surf_pf.txt'))
+#d_pf = np.gradient(pf)
+imglist = ([ag, ia,  ig, pf])
+dv1st = list(map(np.gradient,imglist))
+for img in dv1st:
+    pxx = range(len(img))
+    plt.plot(pxx, img)
+plt.title('1st Deviation of Area of Surface of Crack')
+plt.xlabel('strain')
+plt.ylabel('area of crack surface')
+plt.xlim(xmax=70)
+plt.legend(["ag", "ia", "ig", "pf"],frameon=False)
+plt.savefig(os.path.join(writepath,'1stdiv.pdf'))
+
+plt.show()
+plt.cla
+plt.clf()
+
+
