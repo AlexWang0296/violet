df3fa0f37a0105ab1529928bba82357b765e2226
AlexWang20130296 <severusmas@gmail.com>
2018-09-26 16:03:11 +0800

    add par

--- code/allline.py
+++ code/allline.py
@@ -20,7 +20,8 @@ from os import listdir
 import matplotlib.pyplot as plt
 import pandas as pd
 plt.rcParams["font.family"] = 'Times New Roman'
-
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 12})
 #csfont = {'fontname':'Times New Roman'}
 #plt.rcParams["font.family"] = "Times New Roman"
 WDIR = os.path.dirname(__file__)
@@ -29,20 +30,26 @@ writepath = os.path.join(WDIR,'..','draft','img')
 # pre-set head name
 prop = ['step', 'strain', 'stress', 'energy']
 # import data from .csv file:
-ds1 = pd.read_csv(os.path.join(loadpath,'aa.csv'), sep=' ', skiprows=1, names=prop)
+#ds1 = pd.read_csv(os.path.join(loadpath,'aa.csv'), sep=' ', skiprows=1, names=prop)
 ds2 = pd.read_csv(os.path.join(loadpath,'ag.csv'), sep=' ', skiprows=1, names=prop)
 ds3 = pd.read_csv(os.path.join(loadpath,'pf.csv'), sep=' ', skiprows=1, names=prop)
 ds4 = pd.read_csv(os.path.join(loadpath,'ia.csv'), sep=' ', skiprows=1, names=prop)
+fct = 10e6
+px2=ds2.step/fct
+px3=ds3.step/fct
+px4=ds4.step/fct
+
 # plot lines
-p1 = plt.plot(ds1.step, ds1.stress, label='aa')
-p2 = plt.plot(ds2.step, ds2.stress, label='ag')
-p3 = plt.plot(ds3.step, ds3.stress, label='pf')
-p4 = plt.plot(ds4.step, ds4.stress, label='ia')
+#p1 = plt.plot(ds1.step, ds1.stress, label='aa')
+p2 = plt.plot(px2, ds2.stress, label='ag')
+p3 = plt.plot(px3, ds3.stress, label='pf')
+p4 = plt.plot(px4, ds4.stress, label='ia')
 # config title and label
 
-plt.title('stress-strain curve')
-plt.xlabel('Step')
-plt.xlim(xmax=3e5)
+#plt.title('stress-strain curve')
+plt.xlabel('Strain')
+plt.xlim(xmax=0.035)
+plt.ylim(ymax=6)
 plt.ylabel('Stress (GPa)')
 plt.legend(frameon=False)
 plt.savefig(os.path.join(writepath, 'allline.pdf'))
--- code/data/size/size_summary.py
+++ code/data/size/size_summary.py
@@ -14,20 +14,26 @@ violet
 '''
 import os
 import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 12})
 # plt.rcParams["font.family"] = 'Times New Roman'
 
 WDIR = os.path.dirname(__file__)
 writepath = os.path.join(WDIR,'..','..','..','draft','img')
 
-size = [0, 2, 5, 10, 15, 20]
-iamax = [5.2, 5.16, 5.27, 5.22, 5.19, 4.96]
-agmax = [5.2, 5.19, 5.10, 5.11, 5.09, 4.87]
+size = np.array([0, 2, 5, 10, 15, 20])
+iamax = np.array([5.26, 5.25, 5.25, 5.22, 5.19, 4.96])
+agmax = np.array([5.26, 5.25, 5.17, 5.11, 5.09, 4.87])
+fac = (1-np.pi*(size/2)**2/(200*180))*iamax[0]
 
-plt.plot(size, iamax, linestyle='--', marker='s', color='darkorange')
-plt.plot(size, agmax, linestyle='--', marker='^', color='steelblue')
-
-plt.legend(['ia', 'ag'], frameon=False)
-# plt.ylim(ymin=0)
+plt.plot(size, iamax, linestyle='--', marker='s')
+# color='darkorange'
+# color='steelblue'
+plt.plot(size, agmax, linestyle='--', marker='^')
+plt.plot(size, fac, linestyle='--',marker='o')
+plt.legend(['ia', 'ag','Calculated'], frameon=False)
+plt.ylim(4.75,5.35)
 plt.xlabel('Radius of Void/A')
 plt.ylabel('Strength/GPa')
 plt.savefig(os.path.join(writepath,'effect_of_vol.pdf'))
